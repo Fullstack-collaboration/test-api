3b8db1dbc8d648a554aa09d0ca91b5806c2c42b7
Muhammad Rafai <rafaimhd123@gmail.com>
2023-11-04 23:12:53 +0700

    test env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3123
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send(`r2 a id = ${process.env.R2_SECRET_ACCESS_KEY}`)
 })
 
 app.listen(port, () => {
